cce0af06c16bea2bf2e663213fb2e29c70469ee6
erwi saputra <86191067+erwinsaputraa@users.noreply.github.com>
2023-01-05 21:44:26 +0800

    Add files via upload

---
+++ untitled1/src/main/java/main.java
@@ -0,0 +1,11 @@
+
+import frame.KotaViewFrame;
+import helpers.Koneksi;
+
+public class main {
+    public static void main(String[] args) {
+       // Koneksi.getConnection();
+        KotaViewFrame viewframe = new KotaViewFrame();
+        viewframe.setVisible(true);
+    }
+}
